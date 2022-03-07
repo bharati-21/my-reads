4d1c9b31abbcb957a3e4500e06b584fb4c4a27ad
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-07 22:17:36 +0530

    feat: initial pages for routes

---
+++ src/pages/MyShelf.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyShelf = () => {
+  return (
+    <div>MyShelf</div>
+  )
+}
+
+export {MyShelf}
---
+++ src/pages/MyTbr.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyTbr = () => {
+  return (
+    <div>MyTbr</div>
+  )
+}
+
+export {MyTbr}
