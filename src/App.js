f44ddfe9722d24cf657786590e950856df25249c
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-07 22:17:02 +0530

    feat: Added Routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import './App.css';
-import {Navbar} from './components/Navbar';
-import {useTheme} from './contexts/theme-context';
-import {BookList} from './components/BookList'
+import { BookList } from './pages/BookList'
+import { MyShelf } from './pages/MyShelf';
+import { MyTbr } from './pages/MyTbr';
+import { NotFound } from './pages/NotFound';
+
+import { Navbar } from './components/Navbar';
+import { useTheme } from './contexts/theme-context';
 import { Footer } from './components/Footer';
+
 
 const App = () => {
     const {theme} = useTheme();
     return (
         <div className={`App ${theme}`}>
             <div className="app-container">
-                <Navbar />
-                <BookList  />
-            </div>
+                <BrowserRouter> 
+                    <Navbar />
+                    <Routes>
+                        <Route 
+                            path='*' 
+                            element={<NotFound />} 
+                        />
+                        <Route 
+                            path="/" element={<BookList  />}
+                        />
+                        <Route 
+                            path="/shelf" element={<MyShelf  />}
+                        />
+                        <Route 
+                            path="/tbr" element={<MyTbr  />}
+                        />
+                    </Routes>
+                </BrowserRouter>
+            </div>          
             <Footer />
         </div>
     );
