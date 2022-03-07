d1851cb8b99e4b76668a3b4daa5797996367eb9d
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-07 22:15:13 +0530

    feat: added Link routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,31 +3,33 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import '../styles/navbar.css';
 
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
 import {useTheme} from '../contexts/theme-context';
 
 const Navbar = () => {
     const {theme, toggleTheme} = useTheme();
   return (
-    <nav className="navbar">
-        <h3 className="logo">
-            myReads
-        </h3>
-        <ul className="navlinks">
-            <li className="nav-link">
-                <a href="" className="link">
-                    My Shelf
-                </a>
-            </li>
-            <li className="nav-link">
-                <a href="" className="link">
-                    My TBR
-                </a>
-            </li>
-        </ul>
-        <div className="theme-wrapper" onClick={toggleTheme}>
-            {theme === 'dark' ? <LightModeIcon  /> : <DarkModeIcon /> }
-        </div>
-    </nav>
+        <nav className="navbar">
+            <h3 className="logo">
+               <Link to="/">myReads</Link>
+            </h3>
+            <ul className="navlinks">
+                <li className="nav-link">
+                    <Link to="/shelf">
+                        My Shelf
+                    </Link>
+                </li>
+                <li className="nav-link">
+                <Link to="/tbr">
+                        My TBR
+                    </Link>
+                </li>
+            </ul>
+            <div className="theme-wrapper" onClick={toggleTheme}>
+                {theme === 'dark' ? <LightModeIcon  /> : <DarkModeIcon /> }
+            </div>
+        </nav>
   )
 }
 
