16e2e3fe882167e0e4e14e1539953627ba2617a1
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-07 22:16:34 +0530

    fix: add key to Book item

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="footer">
-        <p class="author">Made with <CodeIcon className="code-icon"/> by Bharati</p>
+        <p className="author">Made with <CodeIcon className="code-icon"/> by Bharati</p>
         <small>All rights reserved. &copy; {new Date().getFullYear()}</small>
     </footer>
   )
