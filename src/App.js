af9eed12c04eaa251d18b63488e8e1199482d839
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-07 17:45:21 +0530

    feat: home Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Navbar} from './components/Navbar';
+import {useTheme} from './contexts/theme-context';
+import {BookList} from './components/BookList'
+import { Footer } from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const {theme} = useTheme();
+    return (
+        <div className={`App ${theme}`}>
+            <div className="app-container">
+                <Navbar />
+                <BookList  />
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
