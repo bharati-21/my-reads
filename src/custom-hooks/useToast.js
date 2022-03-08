f7450295019b3bf208e187f7dc4572d60de3be53
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-08 20:47:12 +0530

    feat: custom hook for toast state

---
+++ src/custom-hooks/useToast.js
@@ -0,0 +1,30 @@
+import { useReducer } from 'react';
+
+const useToast = () => {
+    const toastReducer = (prevToast, action) => {
+        const {type} = action;
+        switch(type){
+            case 'SHOW_TOAST':
+                const {payload} = action;
+                const {toastText, toastTheme} = payload;
+
+                return {...prevToast, showToast: true, toastText, toastTheme };
+            case 'HIDE_TOAST':
+                return {...prevToast, showToast: false, toastText:'', toastTheme:'none' };
+            default:
+                return prevToast;
+        }
+    }
+
+    const intitalToastState = {
+        showToast: false, 
+        toastText: '', 
+        toastTheme: 'none'
+    };
+
+    const [toastState, toastDispatch] = useReducer(toastReducer, intitalToastState);
+
+    return {...toastState, toastDispatch};
+}
+
+export {useToast};
