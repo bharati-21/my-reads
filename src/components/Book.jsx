408ce62b8ce0045d4a1ac5659e193e115111cd74
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-08 11:14:52 +0530

    refactor: add class names to book info

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Book = ({book, handleAddToTBR, handleAddToShelf}) => {
     return (
         <div className="book-container">
-            <img src={book.imageUrl} alt="Book Cover"  />
-            <li>{book.title}</li>
-            <li>{book.author}</li>
+            <img src={book.imageUrl} alt="Book Cover" className="book-cover-img" />
+            <h3 className="book-title">{book.title}</h3>
+            <p className="book-author">{book.author}</p>
             <div className="button-container">
-                <button className="btn btn-secondary" onClick={handleAddToTBR}>Add to TBR</button>
-                <button className="btn btn-accent" onClick={handleAddToShelf}>Add to Shelf</button>
+                <button className="btn btn-secondary btn-add-tbr" onClick={handleAddToTBR}>Add to TBR</button>
+                <button className="btn btn-accent btn-add-shelf" onClick={handleAddToShelf}>Add to Shelf</button>
             </div>
         </div>
     )
