d8756e5953bd75356b55775580a0e59389cb382a
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-08 20:44:26 +0530

    feat: add to tbr, post tbr to json server

--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -1,44 +1,86 @@
 import {useBooks} from '../custom-hooks/useBooks';
 import {Book} from '../components/Book';
 import {SearchForm} from '../components/SearchForm';
-
+import {useTbr} from '../contexts/tbr-context';
+import { useToast } from '../custom-hooks/useToast';
+import { Toast } from '../components/Toast';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const BookList = () => {  
     const {books, loading, error, searchText, dispatch} = useBooks();
 
+    const {tbrState, tbrDispatch} = useTbr();
+
+    const {toastDispatch, toastTheme, toastText, showToast} = useToast();
+
     const getFilteredBooks = () => {
         if(searchText !== "") 
-            return books.filter(book => book.title.toLowerCase().startsWith(searchText.toLowerCase()));
+            return books.filter(book => book.title.toLowerCase().startsWith(searchText.toLowerCase()) || book.title.toLowerCase().includes(searchText.toLowerCase()));
         return books;
     }
 
     const filteredBooks = getFilteredBooks();
 
-    const handleAddToTBR = () => {
-
+    const postDataToTbr = async book => {
+        try {
+            const res = await axios.post('http://localhost:5000/tbr', book);
+            if(res.status >= 200 && res.status < 300) {
+                return true;
+            }
+            throw new Error('Something went really wrong!')
+        }
+        catch(error) {
+            return false;
+        } 
     }
 
-    const handleAddToShelf = () => {
-        
+    const sendDispatchToTbr = (book, type, text) => {
+        tbrDispatch({type, payload: book});
+        if(postDataToTbr(book)) {
+            toastDispatch({type: 'SHOW_TOAST', payload: {toastText: text, toastTheme: 'success'}});
+        }
+        else {
+            toastDispatch({type: 'SHOW_TOAST', payload: {toastText: 'Something went really wrong!', toastTheme: 'error'}});
+        }
+        setTimeout(() => toastDispatch({type: 'HIDE_TOAST'}), 3000);
+    }
+
+    const checkIfBookInTbr = id => {
+        return tbrState.find(tbrBook => tbrBook.id === id);
     }
 
     return (
     <div>
-        <SearchForm dispatch={dispatch} searchText={searchText} />
-        {loading && <h3>{loading}</h3>}
-        {error!=="" && <h3>{error}</h3>}
-        {
-            !loading && !error && 
-            <ul className="booklist-container">
+            {showToast && <Toast toastText={toastText} toastTheme={toastTheme} toastDispatch={toastDispatch} />}
+            <SearchForm dispatch={dispatch} searchText={searchText} />
+            {loading && <h3>{loading}</h3>}
+            {error!=="" && <h3>{error}</h3>}
             {
-                !filteredBooks.length ? 
-                    <div>
-                        No Books Found!
-                    </div> :  
-                filteredBooks.map(book => <Book key={book.id} book={book} handleAddToShelf={handleAddToShelf} handleAddToTBR={handleAddToTBR} />)
+                !loading && !error && 
+                <ul className="booklist-container">
+                {
+                    !filteredBooks.length ? 
+                        <div>
+                            No Books Found!
+                        </div> :  
+                    filteredBooks.map(book => (
+                        <Book key={book.id} book={book}>
+                            {
+                                checkIfBookInTbr(book.id) ? <Link to="/tbr">
+                                    <button className="btn btn-secondary">
+                                        Go to TBR
+                                    </button></Link>
+                                :
+                                <button className="btn btn-secondary btn-add-tbr" onClick={e => sendDispatchToTbr(book, 'ADD_TO_TBR', `Added ${book.title} to TBR`)}>Add to TBR</button>
+                            }
+                            
+                            <button className="btn btn-accent btn-add-shelf" onClick={e => sendDispatchToTbr(book)}>Add to Shelf</button>
+                        </Book>
+                    ))
+                }
+                </ul>
             }
-            </ul>
-        }
     </div>
   )
 }
