aa9ee5ff7c493842afe0331477774836d6bd8640
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-08 20:45:07 +0530

    fix: pass buttons as props to Book card

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 
-const Book = ({book, handleAddToTBR, handleAddToShelf}) => {
+const Book = ({book, children}) => {
     return (
         <div className="book-container">
             <img src={book.imageUrl} alt="Book Cover" className="book-cover-img" />
             <h3 className="book-title">{book.title}</h3>
             <p className="book-author">{book.author}</p>
             <div className="button-container">
-                <button className="btn btn-secondary btn-add-tbr" onClick={handleAddToTBR}>Add to TBR</button>
-                <button className="btn btn-accent btn-add-shelf" onClick={handleAddToShelf}>Add to Shelf</button>
+                {children}
             </div>
         </div>
     )
