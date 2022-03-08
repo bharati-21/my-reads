7a2721911bfd546b6131c0e8366d9dbeeda6a5fd
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-08 11:34:16 +0530

    refactor: moved searchForm handler to SearchForm

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-const SearchForm = ({searchBook, searchText ,dispatch}) => {
+const SearchForm = ({searchText ,dispatch}) => {
+
+    const searchBook = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <form onSubmit={searchBook} className="form">
             <input type="search" placeholder="Search Books..."  required value={searchText} onChange={e => dispatch({type: 'SET_SEARCH_TEXT', payload: e.target.value})} />
