e748840b79d7fd4eec885603a608cb7182a1360a
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-09 15:10:35 +0530

    feat: account information page

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { useAuth } from '../contexts/auth-context';
 
 const MyAccount = () => {
-  return (
-    <div>MyAccount</div>
-  )
+    const { authState } = useAuth();
+
+    const {user : {email, displayName}} = authState;
+
+    return (
+        <div className="account-container">
+            <h2 className="section-head">
+                My Account
+            </h2>   
+            <div className="account-details">
+                <div className="account-info account-user-name">
+                    <h4 className="info-head">Name</h4> <span className="info-value">{displayName}</span>
+                </div> 
+                <div className="account-info account-user-name">
+                    <h4 className="info-head">Email</h4> <span className="info-value">{email}</span>
+                </div>   
+            </div> 
+        </div>
+    );
 }
 
 export {MyAccount};
