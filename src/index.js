56530e800165f0f05589a3fabcb70210f45af464
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-09 10:40:02 +0530

    feat: Added Auth Provider as parent

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {ThemeProvider} from './contexts/theme-context';
 import {TbrProvider} from './contexts/tbr-context';
+import { AuthProvider } from './contexts/auth-context';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider>
-        <TbrProvider>
-            <App />
-        </TbrProvider>
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <AuthProvider>
+            <ThemeProvider>
+                <TbrProvider>
+                    <App />
+                </TbrProvider>
+            </ThemeProvider>
+        </AuthProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
