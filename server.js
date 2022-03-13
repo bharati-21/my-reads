084d44f35afc74aa5c1551be1dde288a2ee2c03d
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-13 13:53:03 +0530

    Heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
+const cors = require('cors');
 const middleware = jsonServer.defaults({
     static: './build',
 });
@@ -13,4 +14,5 @@ server.use(
 )
 
 server.use(router);
+server.use(cors);
 server.listen(port, () => console.log(`Server is running on port ${port}`));
